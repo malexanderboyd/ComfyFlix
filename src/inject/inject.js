de6f93f1dfe2a819b4897c98d1d4d95cb0ee81d9
M. Alex Boyd <mboyd8@students.towson.edu>
2017-01-15 12:57:10 -0500

    Updated for 2017 Netflix Design
    Updated the class for the nxtButton within the mutation observer for
    Netflixs 2017 design.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,11 @@
 
 
-
-	
-	var skipsRemaining = 0;
-	var sleepMode = false;
+var skipsRemaining = 0;
+var sleepMode = false;
+function checkSleepMode()
+{
 	chrome.storage.local.get('counter', function(result) {
 		skipsRemaining = result.counter;
-		
 		if(skipsRemaining > 0)
 		{
 		sleepMode = true;
@@ -14,39 +13,41 @@
 		else
 		{
 		skipsRemaining = 0
-		console.log("No sleep mode enabled. ~ComfyFlix");
 		}
 	});
+}
 
 
 var observer = new MutationObserver(function(mutations) {
-    isItTwo = document.querySelectorAll('.next-episode-container');
+	 if(!sleepMode)
+			checkSleepMode();
+    isItTwo = document.querySelectorAll('.player-postplay');
  	if(isItTwo.length && skipsRemaining == 0 && sleepMode == true)
 	{
 		console.log("Skipped desired number of episodes. Redirecting to homepage... ~ComfyFlix");
 		window.location = "https://netflix.com/";
 		throw new Error("Stopping ComfyFlix.");
 	}
-    	else if (isItTwo.length && skipsRemaining >= 0) 
+    	else if (isItTwo.length && skipsRemaining >= 0)
 	{
-        	var nxtButton = document.querySelectorAll('.nf-big-play-pause.play.grow > button')
+        	var nxtButton = document.querySelectorAll('.player-postplay-show-autoplay > div')
 		nxtButton[0].click();
 		if(skipsRemaining > 0)
 		{
-        		console.log("skipped the countdown for you " + skipsRemaining + " episode(s) remaining before stopping. ~ComfyFlix"); 
+        		console.log("skipped the countdown for you " + skipsRemaining + " episode(s) remaining before stopping. ~ComfyFlix");
 			skipsRemaining--;
 		}
 		else
 		{
 		console.log("Skipped Netflix countdown timer for you. \n <3 ComfyFlix");
 		}
-	
-    	} 
+
+    	}
 });
 var observerConfig = {attributes: true,childList: true,subtree: true,characterData: false};
 var targetNode = document.body;
 setTimeout(function() {
 
     observer.observe(targetNode, observerConfig);
-}, 7500);
-    console.log('ComfyFlix is now active.');
+}, 10500);
+  console.log('ComfyFlix is now active.');
