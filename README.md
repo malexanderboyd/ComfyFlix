e31da978734e9eb0d642e8b5655b08317fbec8b9
M. Alex Boyd <mboyd8@students.towson.edu>
2018-03-18 22:31:02 -0400

    v2.0.5: Small cleanup and updated for 3/18/18 netflix.
    v2.0.5: Small cleanup and updated for 3/18/18 netflix.

---
+++ README.md
@@ -0,0 +1,9 @@
+# ComfyFlix
+
+## The key to great sleep!
+
+
+ComfyFlix is a chrome extension that will skip the countdown timer between episodes on Netflix. 
+
+Also, ComfyFlix features a sleep mode available by right clicking the icon. This allows you to set a counter for the number of episodes to skip before redirecting to Netflix so
+your episodes don't run all night!
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,68 +1,55 @@
-
-
 var skipsRemaining = 0;
 var sleepMode = false;
-function checkSleepMode()
-{
-	chrome.storage.local.get('counter', function(result) {
+function checkSleepMode() {
+	chrome.storage.local.get('counter', function (result) {
 		skipsRemaining = result.counter;
-		if(skipsRemaining > 0)
-		{
-		sleepMode = true;
+		if (skipsRemaining > 0) {
+			sleepMode = true;
 		}
-		else
-		{
-		skipsRemaining = 0
+		else {
+			skipsRemaining = 0
 		}
 	});
 }
 
 
-var observer = new MutationObserver(function(mutations) {
-	 if(!sleepMode)
-			checkSleepMode();
-    var isItTwo = document.querySelectorAll('div.WatchNext');
-	var isItTwo2 = document.querySelectorAll('.nf-flat-button-primary')
- 	if((isItTwo2.length || isItTwo.length) && skipsRemaining == 0 && sleepMode == true)
-	{
+var observer = new MutationObserver(function (mutations) {
+	if (!sleepMode)
+		checkSleepMode();
+	var watchNextContainer = document.querySelectorAll('.WatchNext-still-hover-container');
+	if (watchNextContainer != null && skippRemaining == 0 && sleepMode == true) {
 		console.log("Skipped desired number of episodes. Redirecting to homepage... ~ComfyFlix");
 		window.location = "https://netflix.com/";
 		throw new Error("Stopping ComfyFlix.");
 	}
-    	else if ((isItTwo.length || isItTwo2.length) && skipsRemaining >= 0)
-	{
-        var nxtButton = document.querySelectorAll('div.WatchNext-still-container')
-		var nxtButton2 = document.querySelectorAll('.nf-flat-button-primary')
-		if(nxtButton != null && nxtButton != undefined)
-		try {
-			nxtButton[0].click();
-		} catch(e) {
-			console.debug(e);
-		}
-		if(nxtButton2 != null && nxtButton2 != undefined)
+	else if (watchNextContainer != null && skipsRemaining >= 0) {
+		var playButton = document.querySelectorAll('.PlayIcon')
+		if (playButton != null)
 			try {
-			nxtButton2[0].click();
-			} catch(e) {
-				console.debug(e);
+				playButton[0].click();
+			} catch (e) {
+				try {
+					watchNextContainer[0].click();
+				} catch (e) {
+					console.debug(e);
+				}
 			}
-		if(skipsRemaining > 0)
-		{
-        		console.log("skipped the countdown for you " + skipsRemaining + " episode(s) remaining before stopping. ~ComfyFlix");
+		if (skipsRemaining > 0) {
+			console.log("skipped the countdown for you " + skipsRemaining + " episode(s) remaining before stopping. ~ComfyFlix");
 			skipsRemaining--;
-			nxtButton = undefined
-			nxtButton2 = undefined
+			playButton = undefined
+			watchNextContainer = undefined
 		}
-		else
-		{
-		console.log("Skipped Netflix countdown timer for you. \n <3 ComfyFlix");
+		else {
+			console.log("Skipped Netflix countdown timer for you. \n <3 ComfyFlix");
 		}
 
-    	}
+	}
 });
-var observerConfig = {attributes: true,childList: true,subtree: true,characterData: false};
+var observerConfig = { attributes: true, childList: true, subtree: true, characterData: false };
 var targetNode = document.body;
-setTimeout(function() {
+setTimeout(function () {
 
-    observer.observe(targetNode, observerConfig);
+	observer.observe(targetNode, observerConfig);
 }, 10500);
-  console.log('ComfyFlix is now active.');
+console.log('ComfyFlix is now active.');
