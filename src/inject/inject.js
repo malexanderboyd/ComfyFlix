cb4ade8bd9a94df92705ae84ca2b08393d48f90d
M. Alex Boyd <mboyd8@students.towson.edu>
2018-04-04 23:05:46 -0400

    Skip Intro + Updated Next Episode
    4/4/2018

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -17,22 +17,56 @@ function checkSleepMode() {
 }
 
 
-var observer = new MutationObserver(function (mutations) {
-	if (!sleepMode)
-		checkSleepMode();
-	var watchNextContainer = document.querySelectorAll('.WatchNext-still-hover-container');
-	if (watchNextContainer.length > 0 && skipsRemaining == 0 && sleepMode == true) {
-		console.log("Skipped desired number of episodes. Redirecting to homepage... ~ComfyFlix");
-		window.location = "https://netflix.com/";
-		throw new Error("Stopping ComfyFlix.");
-	}
-	else if (watchNextContainer.length > 0 && skipsRemaining >= 0) {
+function tryClickFlatButton(nextButton, phrase) {
+			try {
+				nextButton.forEach(function(element) {
+					eleAttributes = element.attributes
+					for(var i = 0; i < eleAttributes.length; i++) {
+						var attribute = eleAttributes.item(i);
+						if (attribute.value.includes(phrase) || attribute.value.includes(phrase.capitalize()) || element.innerHTML.includes(phrase) || element.innerHTML.includes(phrase.capitalize())) {
+							element.click();
+							console.log('Skipped ' + phrase.capitalize());
+							break;
+						}
+					}
+					});
+			} catch(e) {
+				console.debug(e);
+			}
+}
+
+function tryClickContainer(watchNextContainer) {
 		try {
 					watchNextContainer[0].click();
 				} catch (e) {
 					console.debug(e);
 				}
-			}
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+var observer = new MutationObserver(function (mutations) {
+	if (!sleepMode)
+		checkSleepMode();
+	var watchNextContainer = document.querySelectorAll('.WatchNext-still-hover-container');
+	var nextButton = document.querySelectorAll('.nf-flat-button-primary');
+	var skipIntro = document.querySelectorAll('.nf-flat-button-text');
+
+	if(skipIntro.length > 0) {
+		tryClickFlatButton(skipIntro, 'intro')
+	}
+
+	if ((nextButton.length > 0 || watchNextContainer.length > 0) && skipsRemaining == 0 && sleepMode == true) {
+		console.log("Skipped desired number of episodes. Redirecting to homepage... ~ComfyFlix");
+		window.location = "https://netflix.com/";
+		throw new Error("Stopping ComfyFlix.");
+	}
+	else if ((nextButton.length > 0 || watchNextContainer.length > 0) && skipsRemaining >= 0) {
+				tryClickContainer(watchNextContainer);
+				tryClickFlatButton(nextButton, 'episode');
+		}
 		if (skipsRemaining > 0) {
 			console.log("skipped the countdown for you " + skipsRemaining + " episode(s) remaining before stopping. ~ComfyFlix");
 			skipsRemaining = skipsRemaining-1;
@@ -43,5 +77,5 @@ var observerConfig = { attributes: true, childList: true, subtree: true, charact
 var targetNode = document.body;
 setTimeout(function () {
 	observer.observe(targetNode, observerConfig);
-}, 12000);
+}, 4000);
 console.log('ComfyFlix is now active.');
