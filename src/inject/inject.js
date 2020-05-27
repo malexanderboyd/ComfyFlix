4b8ca84abc74945a7d8013af3e2af0c82cc17a8a
Alex Boyd <malexanderboyd@gmail.com>
2020-05-26 21:58:16 -0400

    reduce permissions

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,158 +1,29 @@
 async function watch() {
   if (window.location.href.includes("/watch/")) {
-    getSleepSettings().then(async sleepModeSettings => {
-      let sleepMode = sleepModeSettings[0];
-      let skipsRemaining = sleepModeSettings[1];
-      console.debug(
-        `Settings: Sleep Mode: ${sleepMode}\n Skips: ${skipsRemaining}`
-      );
-
-      let buttonsWithText = document.querySelectorAll(".nf-flat-button");
-      let watchNextContainer = document.querySelectorAll(
-        ".WatchNext-still-hover-container"
-      );
-
-      if (tryClickContainer(watchNextContainer)) {
-        const container = watchNextContainer[0];
-        if (sleepMode) {
-            skipsRemaining = await decrementSkips(skipsRemaining);
-            if (shouldSleep(skipsRemaining, sleepMode)) {
-                await returnHome();
-            }
-        }
-          container.click();
-      }
-
-      for (let idx = 0; idx < buttonsWithText.length; idx++) {
-        const curr_button = buttonsWithText[idx];
-        if (curr_button) {
-          if (trySkipIntro(curr_button)) {
-            curr_button.click();
-            break;
-          }
-          if (tryClickFlatButton(curr_button, "next episode")) {
-            if (sleepMode) {
-              skipsRemaining = await decrementSkips(skipsRemaining);
-              if (shouldSleep(skipsRemaining, sleepMode)) {
-                await returnHome();
+    let allButtons = document.querySelectorAll("button")
+    for(let idx = 0; idx < allButtons.length; idx++) {
+      const currButton = allButtons[idx];
+      let skipped = false;
+      if (currButton) {
+          for(let j = 0; j < currButton.attributes.length; j++) {
+            let attribute = currButton.attributes[j];
+            if(attribute.specified) {
+              if (attribute.value.toLocaleLowerCase() === "next-episode-seamless-button") {
+                currButton.click()
+                console.log("click")
+                skipped = true;
+                break;
               }
             }
-              curr_button.click();
+          }
+          if(skipped === true) {
+            break;
           }
         }
       }
-    });
   }
 }
 
 window.setInterval(async () => {
   await watch();
-}, 5000);
-
-async function decrementSkips(skipsRemaining) {
-  const episodeVerbage = skipsRemaining > 1 ? "episodes" : "episode";
-  console.log(
-    `Comfyflix skipped to next episode for you. ${skipsRemaining} ${episodeVerbage} remaining before shutdown.`
-  );
-  return await setSkipsRemaining(skipsRemaining - 1);
-}
-
-const getSleepSettings = () => {
-  return new Promise(resolve => {
-    chrome.storage.local.get("sleepCounter", async result => {
-      const raw = result.sleepCounter || 0;
-      if (isNaN(raw)) {
-        return [false, 0];
-      }
-      const skipsRemaining = parseInt(raw) || 0;
-      if(skipsRemaining > 0) {
-          await setSleepMode(true);
-      }
-      const sleepMode = await getSleepMode();
-
-      resolve([sleepMode, skipsRemaining]);
-    });
-  });
-};
-
-async function setSkipsRemaining(newSkipsRemaining) {
-  return new Promise(resolve => {
-    chrome.storage.local.set({ sleepCounter: newSkipsRemaining }, () => {
-      resolve(newSkipsRemaining);
-    });
-  });
-}
-
-async function getSleepMode() {
-    return new Promise(resolve => {
-        chrome.storage.local.get('sleepMode', result => {
-            const raw = result.sleepMode || false;
-            if(raw !== true && raw !== false) {
-                return false;
-            }
-            resolve(raw)
-        })
-    })
-}
-
-async function setSleepMode(newSleepMode) {
-    return new Promise(resolve => {
-        chrome.storage.local.set({sleepMode: newSleepMode}, () => {
-            resolve(newSleepMode)
-        })
-    })
-}
-
-
-const tryClickFlatButton = (curr_button, phrase) => {
-  const content = curr_button.innerHTML.toLocaleLowerCase();
-  console.debug(`Content: ${content}`);
-  console.debug(`Phrase: ${phrase}`);
-  const searchRegEx = new RegExp(phrase);
-  if (searchRegEx.test(content) && curr_button) {
-    return true;
-  }
-  return false;
-};
-
-const tryClickContainer = watchNextContainer => {
-  try {
-    if (window.location.href.includes("/watch/")) {
-      const container = watchNextContainer[0];
-      if (container) {
-        return true;
-      }
-    }
-    return false;
-  } catch (e) {
-    console.debug(e);
-  }
-};
-
-const shouldSleep = (skipsRemaining, sleepModeActive) => {
-  return skipsRemaining < 0 && sleepModeActive;
-};
-
-const returnHome = async () => {
-  console.log(
-    "Skipped desired number of episodes. Redirecting to homepage... ~ComfyFlix"
-  );
-  window.location.replace("https://netflix.com/");
-  await setSleepMode(false);
-  return true
-};
-
-const trySkipIntro = curr_button => {
-  return tryClickIntroButton(curr_button);
-};
-
-const tryClickIntroButton = curr_button => {
-  if(curr_button.hasAttributes()) {
-    let attrs = curr_button.attributes;
-    for(var i = attrs.length - 1; i >= 0; i--) {
-      if(attrs[i].name.toLocaleLowerCase() === 'aria-label' && attrs[i].value.toLocaleLowerCase() === 'skip intro') {
-        return true;
-      }
-    }
-  }
-}
+}, 1000);
